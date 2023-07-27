858b99183d2765d9a8fbb597ed4521a6bd9fe165
Damian Solis <damianrsolis@gmail.com>
2023-07-27 15:33:42 -0500

    HelloWorld.java exercise completed

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System is a class, out is a method, println is the "console.log() of Java"
+//        short hand of .log<tab key> is sout<tab key>
+        System.out.println("Hello, Ent!");
+        System.out.print("little Hi, little Hoe");
+    }
+}
